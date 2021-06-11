5883c95155e3fb3f8f339cf23751b17f2fe63f85
Meslmawy <khaledsherif296@gmail.com>
2021-06-11 10:31:40 +0200

    change variables names

--- app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/data/models/ArticleModelResponse.kt
+++ app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/data/models/ArticleModelResponse.kt
@@ -1,6 +1,7 @@
 package com.ibtikar.mvvm_starter_koin_coroutines.data.models
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 import java.math.BigInteger
 
@@ -13,14 +14,17 @@ data class ArticleModelResponse(
     val uri : String,
     val url : String,
     val id : BigInteger,
-    val asset_id : BigInteger,
+    @SerializedName("asset_id")
+    val assetId : BigInteger,
     val source : String,
-    val published_date : String,
+    @SerializedName("published_date")
+    val publishedDate : String,
     val updated : String,
     val section : String,
     val subsection : String,
     val nytdsection : String,
-    val adx_adx_keywordswords : String,
+    @SerializedName("adx_adx_keywordswords")
+    val adxKeywordswords : String,
     val column : String,
     val byline : String,
     val type : String,
